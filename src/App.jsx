d10cd0a45fb44dae7cee9b5e8c1a23851c870d8a
PatrickBett <patrickbett018@gmail.com>
2023-11-06 17:27:34 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import FetchedBots from "./components/FetchedBots";
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      
+      <FetchedBots />
+    </div>
+  );
+}
+
+export default App;
